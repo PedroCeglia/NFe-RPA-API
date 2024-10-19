c25ff3ab7aec86a738125b990d50946d81984bbb
PedroCeglia <dev.ceglia.pedro@gmail.com>
2024-10-18 23:59:35 -0300

    remove origins from cors

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from NFePlanilhaFinal.main import create_planilhas_by_danfe
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/processar_arquivos": {"origins": "https://6z4wqd.csb.app"}})
+#CORS(app, resources={r"/processar_arquivos": {"origins": "https://6z4wqd.csb.app"}})
+CORS(app)
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
